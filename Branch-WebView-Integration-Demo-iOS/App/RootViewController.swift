8eec80f0b7f7b1a4c374f45c38586fa3dc6fdbba
Michael Huber <mike.huber@branchapp.com>
2020-11-18 10:10:15 -0500

    setup RootViewController as entry point

---
+++ Branch-WebView-Integration-Demo-iOS/App/RootViewController.swift
@@ -0,0 +1,56 @@
+//
+//  RootViewController.swift
+//  Branch-WebView-Integration-Demo-iOS
+//
+//  Created by Michael Huber on 11/18/20.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    // MARK: - Views
+
+    private lazy var launchButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Launch WebView", for: .normal)
+        button.addTarget(
+            self,
+            action: #selector(launchWebView),
+            for: .touchUpInside
+        )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureUI()
+        self.configureConstraints()
+    }
+
+    // MARK: - Layout
+
+    private func configureUI() {
+        self.view.backgroundColor = .systemBackground
+        self.view.addSubview(self.launchButton)
+    }
+
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            self.launchButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.launchButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+    }
+
+    // MARK: - Actions
+
+    @objc private func launchWebView() {
+        let vc = BranchWebViewController()
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true)
+    }
+
+}
